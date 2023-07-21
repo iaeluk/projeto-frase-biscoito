93cb0066e788fb801d75303d6cb6b0ab83f22dad
Lucas Mendonça <lucas21rio@gmail.com>
2023-07-21 12:18:53 -0300

    TailwindCSS e card

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  frases: string[] = [
+    'A vida é boa.',
+    'Nunca desista dos seus sonhos.',
+    'Aprenda algo novo todos os dias.',
+    'Seja grato por tudo que tem.',
+  ];
+  fraseAleatoria: string = '';
+  mostrarFrase: boolean = false;
+
+  exibirFrase() {
+    this.fraseAleatoria = this.frases[Math.floor(Math.random() * this.frases.length)];
+    this.mostrarFrase = true;
+  }
 }
