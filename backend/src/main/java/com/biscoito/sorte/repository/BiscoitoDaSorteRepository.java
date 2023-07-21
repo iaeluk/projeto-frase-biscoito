f2e492b799a9b8b7d5986be051490f65f4172e98
Talita Rosa <talita.rosa@capgemini.com>
2023-07-21 15:15:03 -0300

    removing files

--- backend/src/main/java/com/biscoito/sorte/repository/BiscoitoDaSorteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.biscoito.sorte.repository;
-
-import com.biscoito.sorte.entity.BiscoitoDaSorte;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BiscoitoDaSorteRepository extends JpaRepository<BiscoitoDaSorte, Long> {
-}
