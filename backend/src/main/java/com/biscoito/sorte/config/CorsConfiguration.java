96002ccf24f211fe82cf7d45efd5fd01478c2548
Lucas Mendonça <79822706+iaeluk@users.noreply.github.com>
2023-07-25 21:02:48 -0300

    Update CorsConfiguration.java

--- backend/src/main/java/com/biscoito/sorte/config/CorsConfiguration.java
+++ backend/src/main/java/com/biscoito/sorte/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://localhost:8080", "https://biscoito-da-sorte-59c63b6adf76.herokuapp.com")
+                .allowedOrigins("http://localhost:4200", "http://localhost:8080", "https://projeto-frase-biscoito.vercel.app/", "https://biscoito-da-sorte-59c63b6adf76.herokuapp.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
